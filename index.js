d0b9a2e6d0560fdccbdfb68ed18789522c7ab273
alessandro <alessandro@tembo.it>
2020-02-11 18:00:32 +0100

    commit

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const readlineSync = require('readline-sync')
 const { downloadFunctions } = require('./lib/lambda-download')
 const { setLayers } = require('./lib/layers-set')
+const { updateLayerVersion } = require('./lib/layer-update')
 
 const menu = async () => {
   console.log('> Lambda functions exec')
-  const items = ['Set layers', 'Download selected function', 'Download all functions']
+  const items = ['Set layers', 'Update layer version', 'Download selected function', 'Download all functions']
   let index = readlineSync.keyInSelect(items, '> Select the task', { cancel: 'Exit' })
   switch (parseInt(index)) {
     case 0:
       await setLayers()
       break
     case 1:
+      await updateLayerVersion()
+      break
+    case 2:
       const functionName = readlineSync.question('> Lambda function name: ')
       await downloadFunctions(functionName)
       break
-    case 2:
+    case 3:
       await downloadFunctions()
       break
     default :
